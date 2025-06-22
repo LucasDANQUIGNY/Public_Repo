645b05f830249d86d6dc9b9be8ea78b554f3ebf7
Lucas DANQUIGNY <lucas.dan.08022008@gmail.com>
2025-06-22 18:44:51 +0200

    ip

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-from math import sqrt
+import socket
+from functions import post
 
-print(sqrt(49))
-print("You are cooked!")
+
+def get_ip():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return ip_address
+
+ip = get_ip()
+print("Adresse IP :", ip)
+post("https://serveur-api-xw0q.onrender.com/save", {"ip" : ip})
